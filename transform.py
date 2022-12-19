34298e88ea7c778aa068f19c66c1b25cb969b538
FabianPalaciosGatriaSAS <fabian.palacios@gatria.co>
2022-12-19 08:07:42 -0500

    Cambios 19 Dic de 2022

---
+++ transform.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+def data_normalization(df):
+    pass
---
+++ validate.py
@@ -0,0 +1,272 @@
+import pandas as pd
+
+def validador_1():
+    pass
+
+def validador_2():
+    pass
+
+def validador_3():
+    pass
+
+def validador_4():
+    pass
+
+def validador_5():
+    pass
+
+def validador_6():
+    pass
+
+def validador_7():
+    pass
+
+def validador_8():
+    pass
+
+def validador_9():
+    pass
+
+def validador_10():
+    pass
+
+def validador_1():
+    pass
+
+def validador_2():
+    pass
+
+def validador_3():
+    pass
+
+def validador_4():
+    pass
+
+def validador_5():
+    pass
+
+def validador_6():
+    pass
+
+def validador_7():
+    pass
+
+def validador_8():
+    pass
+
+def validador_9():
+    pass
+
+def validador_10():
+    pass
+
+def validador_1():
+    pass
+
+def validador_2():
+    pass
+
+
+def validador_3():
+    pass
+
+def validador_4():
+    pass
+
+def validador_5():
+    pass
+
+def validador_6():
+    pass
+
+def validador_7():
+    pass
+
+def validador_8():
+    pass
+
+def validador_9():
+    pass
+
+def validador_10():
+    pass
+
+def validador_1():
+    pass
+
+def validador_2():
+    pass
+
+def validador_3():
+    pass
+
+def validador_4():
+    pass
+
+def validador_5():
+    pass
+
+def validador_6():
+    pass
+
+def validador_7():
+    pass
+
+def validador_8():
+    pass
+
+def validador_9():
+    pass
+
+def validador_10():
+    pass
+def validador_1():
+    pass
+
+def validador_2():
+    pass
+
+def validador_3():
+    pass
+
+def validador_4():
+    pass
+
+def validador_5():
+    pass
+
+def validador_6():
+    pass
+
+def validador_7():
+    pass
+
+def validador_8():
+    pass
+
+def validador_9():
+    pass
+
+def validador_10():
+    pass
+
+def validador_1():
+    pass
+
+def validador_2():
+    pass
+
+def validador_3():
+    pass
+
+def validador_4():
+    pass
+
+def validador_5():
+    pass
+
+def validador_6():
+    pass
+
+def validador_7():
+    pass
+
+def validador_8():
+    pass
+
+def validador_9():
+    pass
+
+def validador_10():
+    pass
+
+def validador_1():
+    pass
+
+def validador_2():
+    pass
+
+
+def validador_3():
+    pass
+
+def validador_4():
+    pass
+
+def validador_5():
+    pass
+
+def validador_6():
+    pass
+
+def validador_7():
+    pass
+
+def validador_8():
+    pass
+
+def validador_9():
+    pass
+
+def validador_10():
+    pass
+
+def validador_1():
+    pass
+
+def validador_2():
+    pass
+
+def validador_3():
+    pass
+
+def validador_4():
+    pass
+
+def validador_5():
+    pass
+
+def validador_6():
+    pass
+
+def validador_7():
+    pass
+
+def validador_8():
+    pass
+
+def validador_9():
+    pass
+
+def validador_10():
+    pass
+
+def validador_1():
+    pass
+
+def validador_2():
+    pass
+
+def validador_3():
+    pass
+
+def validador_4():
+    pass
+
+def validador_5():
+    pass
+
+def validador_6():
+    pass
+
+def validador_7():
+    pass
+
+def validador_8():
+    pass
+
+def validador_9():
+    pass
+
+def validador_10():
+    pass
